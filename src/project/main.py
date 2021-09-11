f7975678c6ba5ecf3eaff52f4eab7653dc02832f
Timofey Kukushkin <t.kukushkin@cian.ru>
2021-09-12 00:56:18 +0300

    Make example smaller

--- src/project/main.py
+++ src/project/main.py
@@ -1,15 +1,15 @@
-from project import package
-from project.package import foo_async, foo_sync
+from project import module
+from project.module import foo_async, foo_sync
 
 
 async def main() -> None:
     res1 = await foo_async(1)  # OK
-    res2 = await package.foo_async(1)  # NOT OK
+    res2 = await module.foo_async(1)  # NOT OK
     res3 = await foo_async()  # OK
-    res4 = await package.foo_async()  # OK
+    res4 = await module.foo_async()  # OK
 
     res5 = await foo_async.call(1)  # OK
-    res6 = await package.foo_async.call(1)  # OK
+    res6 = await module.foo_async.call(1)  # OK
 
     res7 = foo_sync(1)  # OK
-    res8 = package.foo_sync(1)  # NOT OK
+    res8 = module.foo_sync(1)  # NOT OK
